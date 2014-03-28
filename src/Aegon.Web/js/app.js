c60fc53113ee9421b0d559ca329a0e58394fecd1
Ronglin <ronglin@kooboo.com>
2014-03-28 14:52:48 +0800

    rename partials to views

--- src/Aegon.Web/js/app.js
+++ src/Aegon.Web/js/app.js
@@ -17,12 +17,12 @@
     ]);
 
     aegon.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/home', { templateUrl: 'partials/home.html', controller: 'HomeCtrl' });
-        $routeProvider.when('/user/:productId', { templateUrl: 'partials/user.html', controller: 'UserCtrl' });
-        $routeProvider.when('/suppliers/:productId', { templateUrl: 'partials/suppliers.html', controller: 'SuppliersCtrl' });
-        $routeProvider.when('/compare/:productId/:supplierIds', { templateUrl: 'partials/compare.html', controller: 'CompareCtrl' });
-        $routeProvider.when('/content', { templateUrl: 'partials/content.html' });
-        $routeProvider.when('/redirected', { templateUrl: 'partials/redirected.html' });
+        $routeProvider.when('/home', { templateUrl: 'views/home.html', controller: 'HomeCtrl' });
+        $routeProvider.when('/user/:productId', { templateUrl: 'views/user.html', controller: 'UserCtrl' });
+        $routeProvider.when('/suppliers/:productId', { templateUrl: 'views/suppliers.html', controller: 'SuppliersCtrl' });
+        $routeProvider.when('/compare/:productId/:supplierIds', { templateUrl: 'views/compare.html', controller: 'CompareCtrl' });
+        $routeProvider.when('/content', { templateUrl: 'views/content.html' });
+        $routeProvider.when('/redirected', { templateUrl: 'views/redirected.html' });
         $routeProvider.otherwise({ redirectTo: '/home' });
     }]);
 
