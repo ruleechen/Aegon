b1fd1c933fb7aeed103864459d93bd1bd0604e16
Ronglin-Kooboo <ronglin@kooboo.com>
2014-04-14 11:17:10 +0800

    new gulp file

--- src/Aegon.Web/gulpfile.js
+++ src/Aegon.Web/gulpfile.js
@@ -12,8 +12,9 @@ var gulp = require('gulp');
     ngmin = require('gulp-ngmin'),
     server = lr();
 
-//Scripts
-gulp.task('scripts', function(){
+
+// JS
+gulp.task('js', function(){
   gulp
     .src('js/app.js')
     .pipe(browserify({
@@ -21,22 +22,12 @@ gulp.task('scripts', function(){
       debug : true
     }))
     .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('./js'));
-});
-
-
-// Less
-gulp.task('less', function () {
-  gulp
-    .src('css/main.less')
-    .pipe(less({
-      sourceMap: true
-    }))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('./dist/js'))
     .pipe(livereload(server));
 });
 
-gulp.task('production', function () {
+// JS MIN
+gulp.task('js-min', function () {
   gulp
     .src('js/app.js')
     .pipe(browserify({
@@ -47,25 +38,50 @@ gulp.task('production', function () {
     .pipe(rename({ suffix: '.min' }))
     .pipe(ngmin())
     // .pipe(uglify())
-    .pipe(gulp.dest('./js'));
+    .pipe(gulp.dest('./dist/js'));
+});
 
+// CSS
+gulp.task('css', function () {
+  gulp
+    .src('css/main.less')
+    .pipe(less({
+      sourceMap: true
+    }))
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(livereload(server));
+});
+
+// CSS MIN
+gulp.task('css-min', function () {
   gulp
     .src('css/main.less')
     .pipe(less({
       sourceMap: false
     }))
+    .pipe(concat('bundle.css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+
+
+// Publish
+gulp.task('publish', function () {
+  gulp.run('js');
+  gulp.run('js-min');
+  gulp.run('css');
+  gulp.run('css-min');
 });
 
 
 // Clean
 gulp.task('clean', function() {
-  gulp.src(['./js/bundle.js',
-                   './js/bundle.min.js',
-                   './css/main.min.css',
-                   './css/main.css'], {read: false})
+  gulp.src(['./dist/js/bundle.js',
+            './dist/js/bundle.min.js',
+            './dist/css/bundle.css',
+            './dist/css/bundle.min.css'], {read: false})
     .pipe(clean());
 });
 
@@ -73,18 +89,22 @@ gulp.task('clean', function() {
 // Watch
 gulp.task('watch', function () {
   var paths = {
-    scripts: ['js/**/*.js', 'test/fixtures/*.js'],
-    styles: ['css/**/*.less']
+    scripts: ['js/**/*.js'],
+    styles: ['css/**/*.less', 'css/**/*.css']
   };
   server.listen(35729, function (err) {
     if (err) {
       return console.log(err)
     };
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles, ['less']);
+    gulp.watch(paths.scripts, ['js']);
+    gulp.watch(paths.styles, ['css']);
   });
 });
+
 
 // Default task
 gulp.task('default', ['watch']);
 
+// Run
+gulp.run('js');
+gulp.run('css');
